da370ff9c6616c3c2386d26c31c76a3eb29de6bd
BocognanoSarah <sarah.bocognano@filigran.io>
2023-11-09 10:51:57 +0100

    [frontend] Add location creation (#issue/4832)

--- opencti-platform/opencti-front/src/private/components/threats/threat_actors_individual/AddLocationsThreatActorIndividual.jsx
+++ opencti-platform/opencti-front/src/private/components/threats/threat_actors_individual/AddLocationsThreatActorIndividual.jsx
@@ -102,8 +102,7 @@ class AddLocationsThreatActorIndividual extends Component {
           display={this.state.open}
           contextual={true}
           inputValue={this.state.search}
-          paginationOptions={paginationOptions}
-         onlyAuthors/>
+         />
       </>
     );
   }
